58aedef06a78bd65a7c9d2fd5228c58305d9af82
linearza <vanwijk.mc@gmail.com>
2025-01-17 08:04:31 +0200

    chore: about page embedded code and template syntaxes

--- lib/pensieve_web/controllers/page_controller.ex
+++ lib/pensieve_web/controllers/page_controller.ex
@@ -8,6 +8,17 @@ defmodule PensieveWeb.PageController do
   end
 
   def about(conn, _params) do
+
+    # conn = conn
+    # |> assign(:message, "Hello")
+    # |> assign(:other_message, "Hola")
+
+    conn = conn
+          |> assign(:message, "Hello")
+          |> assign(:other_message, "From the other side")
+
+    # conn = merge_assigns(conn, message: "Hello", other_message: "Hola")
+
     render(conn, :about)
   end
 end
