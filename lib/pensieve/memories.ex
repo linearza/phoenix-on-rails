9de1f2bc77250e969ae34fdf657505cbfda387d1
linearza <vanwijk.mc@gmail.com>
2025-01-23 06:25:25 +0200

    feat: add Memories show page and context

---
+++ lib/pensieve/memories.ex
@@ -0,0 +1,7 @@
+defmodule Pensieve.Memories do
+  alias Pensieve.Repo
+  alias Pensieve.Memories.Memory
+
+  def list_memories, do: Repo.all(Memory)
+  def get_memory!(id), do: Repo.get!(Memory, id)
+end
