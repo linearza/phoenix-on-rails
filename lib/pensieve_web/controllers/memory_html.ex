01cd7045808c6dafaba00fb50768c12df69b97c8
linearza <vanwijk.mc@gmail.com>
2025-01-27 07:42:28 +0200

    feat: create memory_inserted_at method and implement in index and show

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -4,6 +4,12 @@ defmodule PensieveWeb.MemoryHTML do
 
   embed_templates "memory_html/*"
 
+  alias Pensieve.Memories.Memory
+
+  def memory_inserted_at(%Memory{inserted_at: timestamp}) do
+    Calendar.strftime(timestamp, "%m/%d/%Y %I:%M%p")
+  end
+
   # attr :memories, :list, default: []
 
   # defp information(assigns) do
