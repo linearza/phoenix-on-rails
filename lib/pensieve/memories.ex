d8c9297143994b6a46c392e2dd43baef8ce20a9f
linearza <vanwijk.mc@gmail.com>
2025-02-23 12:58:03 +0200

    feat: add new fallback controller and proper error handling

--- lib/pensieve/memories.ex
+++ lib/pensieve/memories.ex
@@ -9,6 +9,14 @@ defmodule Pensieve.Memories do
   @spec get_memory!(pos_integer | String.t()) :: %Memory{}
   def get_memory!(id), do: Repo.get!(Memory, id)
 
+  @spec fetch_memory(id) :: {:ok, %Memory{}} | {:error, atom}
+  def fetch_memory(id) do
+    case Repo.get(Memory, id) do
+      %Memory{} = memory -> {:ok, memory}
+      nil -> {:error, :not_found}
+    end
+  end
+
   @type memory_op_result :: {:ok, %Memory{}} | {:error, %Ecto.Changeset{}}
   @spec create_memory(map) :: memory_op_result
   def create_memory(attrs \\ %{}) do
@@ -25,6 +33,14 @@ defmodule Pensieve.Memories do
   end
 
   @spec change_memory(%Memory{}, map) :: %Ecto.Changeset{}
+  @spec change_memory(%Pensieve.Memories.Memory{
+          __meta__: any(),
+          content: any(),
+          id: any(),
+          inserted_at: any(),
+          title: any(),
+          updated_at: any()
+        }) :: Ecto.Changeset.t()
   def change_memory(%Memory{} = memory, attrs \\ %{}) do
     Memory.changeset(memory, attrs)
   end
--- lib/pensieve_web/controllers/api/memory_controller.ex
+++ lib/pensieve_web/controllers/api/memory_controller.ex
@@ -4,36 +4,68 @@ defmodule PensieveWeb.API.MemoryController do
   alias Pensieve.Memories
   alias Pensieve.Memories.Memory
 
+  action_fallback PensieveWeb.API.FallbackController
+
   def index(conn, _params) do
     memories = Memories.list_memories()
     render(conn, :index, memories: memories)
   end
 
   def show(conn, %{"id" => id}) do
-    memory = Memories.get_memory!(id)
-    render(conn, :show, memory: memory)
+    # memory = Memories.get_memory!(id)
+    # render(conn, :show, memory: memory)
+
+    # case Memories.fetch_memory(id) do
+    #   {:ok, memory} -> render(conn, :show, memory: memory)
+
+    #   {:error, :not_found} ->
+    #     conn
+    #     |> put_status(:not_found)
+    #     |> json(%{error: "Not found"})
+    # end
+
+    with {:ok, memory} <- Memories.fetch_memory(id) do
+      render(conn, :show, memory: memory)
+    end
   end
 
   def create(conn, %{"memory" => memory_params}) do
-    {:ok, %Memory{} = memory} = Memories.create_memory(memory_params)
+    # {:ok, %Memory{} = memory} = Memories.create_memory(memory_params)
 
-    conn
-    |> put_status(:created)
-    |> put_resp_header("location", ~p"/api/memories/#{memory}")
-    |> render(:show, memory: memory)
+    # conn
+    # |> put_status(:created)
+    # |> put_resp_header("location", ~p"/api/memories/#{memory}")
+    # |> render(:show, memory: memory)
+
+    with {:ok, memory} <- Memories.create_memory(memory_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", ~p"/api/memories/#{memory}")
+      |> render(:show, memory: memory)
+    end
   end
 
   def update(conn, %{"id" => id, "memory" => memory_params}) do
-    memory = Memories.get_memory!(id)
+    # memory = Memories.get_memory!(id)
 
-    {:ok, %Memory{} = memory} = Memories.update_memory(memory, memory_params)
-    render(conn, :show, memory: memory)
+    # {:ok, %Memory{} = memory} = Memories.update_memory(memory, memory_params)
+    # render(conn, :show, memory: memory)
+
+    with {:ok, memory} <- Memories.fetch_memory(id),
+      {:ok, memory} <- Memories.update_memory(memory, memory_params) do
+      render(conn, :show, memory: memory)
+    end
   end
 
   def delete(conn, %{"id" => id}) do
-    memory = Memories.get_memory!(id)
+    # memory = Memories.get_memory!(id)
 
-    {:ok, %Memory{} = memory} = Memories.delete_memory(memory)
-    send_resp(conn, :no_content, "")
+    # {:ok, %Memory{} = memory} = Memories.delete_memory(memory)
+    # send_resp(conn, :no_content, "")
+
+    with {:ok, memory} <- Memories.fetch_memory(id),
+      {:ok, _memory} = Memories.delete_memory(memory) do
+      send_resp(conn, :no_content, "")
+    end
   end
 end
